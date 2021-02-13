920a7938c75132ff9903ed5a1657bdcb20eac2e3
samyhaff <haffoudhisamy@gmail.com>
2021-02-13 13:24:16 +0100

    first commit

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <curses.h>
+#include <ncurses.h>
+
+#define WIDTH 80
+#define HEIGHT 24
+#define FPS 60
+
+struct player {
+    int position_x;
+    int position_y;
+    int lives;
+    int fired;
+    int missile_x;
+    int missile_y;
+    char *sprite;
+} player = {WIDTH / 2, HEIGHT - 2, 3, false, 0, 0, "|-^-|"};
 
 int main(int argc, char **argv) {
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
 
+    while (true) {
+        mvprintw(player.position_y, player.position_x, player.sprite);
+        refresh(); 
+    }
+    
+    endwin();
+    
     return 0;
 }
+
