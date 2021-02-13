d0832e7e68dc99350ee23400132fd131e641bc28
samyhaff <haffoudhisamy@gmail.com>
2021-02-13 15:16:22 +0100

    added missile and fps control

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <unistd.h>
+#include <time.h>
 
 #define WIDTH 80
 #define HEIGHT 24
-#define FPS 60
+#define FPS 50
+#define MISSILE_FREQ 200
+
+bool playing = true;
+int counter = 0;
 
 struct player {
     int position_x;
@@ -14,7 +20,55 @@ struct player {
     int missile_x;
     int missile_y;
     char *sprite;
-} player = {WIDTH / 2, HEIGHT - 2, 3, false, 0, 0, "|-^-|"};
+    char *missile_sprite;
+} player = {WIDTH / 2, HEIGHT - 2, 3, false, 0, 0, "|-^-|", "|"};
+
+void getAndRunInput() {
+    int ch = getch();
+    switch (ch) {
+        case 'j':
+            player.position_x -= 1;
+            break;
+        case 'k':
+            player.position_x += 1;
+            break;
+        case 'q':
+            playing = false;
+            break;
+        case 'l':
+            if (!player.fired) {
+                player.fired = true;
+                player.missile_x = player.position_x;
+                player.missile_y = player.position_y;
+            }
+            break;
+        default: 
+            break;
+    }
+}
+
+void update_missile() {
+    if (player.fired) {
+        if (counter == 0)
+            player.missile_y -= 1;
+        if (player.missile_y == 0)
+            player.fired = false;
+        counter = (counter + 1) % MISSILE_FREQ;
+    }
+}
+
+void draw_screen() {
+    clear();
+
+    // draw player
+    mvprintw(player.position_y, player.position_x - 2, player.sprite);
+
+    // draw missile
+    if (player.fired)
+        mvprintw(player.missile_y, player.missile_x, player.missile_sprite);
+
+    refresh(); 
+}
 
 int main(int argc, char **argv) {
     initscr();
@@ -25,19 +79,16 @@ int main(int argc, char **argv) {
     scrollok(stdscr, TRUE);
     nodelay(stdscr, TRUE);
 
-    while (true) {
-        int ch = getch();
-        if (ch == KEY_LEFT)
-            player.position_x -= 1;
-        else if (ch == KEY_RIGHT)
-            player.position_x += 1;
-        clear();
-        mvprintw(player.position_y, player.position_x - 2, player.sprite);
-        refresh(); 
+    while (playing) {
+        clock_t begin = clock();
+        getAndRunInput();
+        update_missile();
+        draw_screen();
+        clock_t end = clock();
+        double time_spent = (double)(end - begin) / CLOCKS_PER_SEC; 
+        sleep((1 / (double) FPS) - time_spent);
     }
     
     endwin();
-    
     return 0;
 }
-
